f25067d128abab5164303a31de85f341be0d52f5
Kottegoda1234 <thilinakottegoda077gmail.com>
2024-10-20 20:49:13 +0530

    edit delete function

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,5 +6,17 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function delete($id)
+{
+    // Find the blog post by ID or throw a 404 error if not found
+    $blog = Blog::findOrFail($id);
+    
+    // Delete the blog post
+    $blog->delete();
+
+    // Optionally, return a success message
+    return response()->json([
+        'message' => 'Blog post deleted successfully!',
+    ], 200);
+}
 }
