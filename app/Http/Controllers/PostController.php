9ca81ae9825fd730af4654b3ede92ae64e3f6f42
nethmiudara2001 <udaranethmi11@gmail.com>
2024-10-31 20:34:35 +0530

    Create Function

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,8 +1,21 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\Posts;
+
+use App\Models\Posts; 
+use App\Http\Requests\PostRequest; 
 use Illuminate\Http\Request;
 
-}
+class PostController extends Controller
+{
+    public function store(PostRequest $request)
+    {
+        Posts::create($request->validated());
+        return redirect()->route('posts.index')->with('success', 'Post created successfully!');
+    }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
 }
