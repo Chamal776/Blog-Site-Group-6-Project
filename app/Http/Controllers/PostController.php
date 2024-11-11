b8e00fbe25ea1ea68c4affd3074802abcac9a445
Chamal776 <chamalharshana2020@gmail.com>
2024-11-11 19:25:03 +0530

    PostController Changed

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,7 +6,7 @@ namespace App\Http\Controllers;
 use App\Models\Posts; 
 use App\Http\Requests\PostRequest; 
 use Illuminate\Http\Request;
-use Illuminate\Http\Request;
+
 
 class PostController extends Controller
 {
