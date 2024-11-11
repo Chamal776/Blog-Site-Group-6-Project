a885ce05dc630ff03bac51f3bedd7fbcfbeaa25a
Chamal776 <chamalharshana2020@gmail.com>
2024-11-11 20:17:00 +0530

    removed error code

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-SEU/IS/20/ICT/007
 use App\Models\Posts; 
 use App\Http\Requests\PostRequest; 
 use Illuminate\Http\Request;
@@ -10,25 +9,7 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function store(PostRequest $request)
-    {
-        Posts::create($request->validated());
-        return redirect()->route('posts.index')->with('success', 'Post created successfully!');
-    }
-
-    public function create()
-    {
-        return view('posts.create');
-    }
-}
+    //
+} 
 
 
-class PostController extends Controller
-   // Edit function
-   public function edit($id)
-   {
-       $post = Posts::findOrFail($id);
-       return response()->json($post); // Use $post instead of $blog
-   } 
-
- main
