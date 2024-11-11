76d7284d2cb4367f9f2199377417a31f6cb05028
Ishani019 <ishanibandara02@gmail.com>
2024-11-11 20:12:50 +0530

    edit funtion

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,12 +3,40 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Posts; 
 
 class PostController extends Controller
-   // Edit function
-   public function edit($id)
-   {
-       $post = Posts::findOrFail($id);
-       return response()->json($post); // Use $post instead of $blog
-   } 
+{
+    // Edit function
+    public function edit($id)
+    {   
+       
+        $post = Posts::findOrFail($id);
+
+   
+        return view('posts.edit', compact('post'));
+    }
+
+    // Update function
+    public function update(Request $request, $id)
+    {
+       
+        $post = Posts::findOrFail($id);
+
+      
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'content' => 'required|string',
+        ]);
+
+      
+        $post->title = $request->input('title');
+        $post->content = $request->input('content');
+        
+      
+        $post->save();
+
+       
+        return redirect()->route('posts.index')->with('success', 'Post updated successfully.');
+    }
 }
