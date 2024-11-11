8f229d60a3a5dbdc40abb28ed283679c5756f91a
Chamal776 <chamalharshana2020@gmail.com>
2024-11-11 21:12:28 +0530

    Routes changed

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,26 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Blog routes
+
+// To show a list of all blogs
+Route::get('blog', [PostController::class, 'index']);
+
+// To show the form for creating a new blog post
+Route::get('blog/create', [PostController::class, 'create']);
+
+// To store a new blog post
+Route::post('blog/store', [PostController::class, 'store']);
+
+// To show the form for editing a blog post
+Route::get('blog/{id}/edit', [PostController::class, 'edit']);
+
+// To update an existing blog post
+Route::post('blog/{id}', [PostController::class, 'update']);
+
+// To delete a blog post
+Route::delete('blog/{id}', [PostController::class, 'delete']);
+
+// To display all blog posts (if needed as a separate view)
+Route::get('blog/all', [PostController::class, 'all']); 
+
